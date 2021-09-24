ba6a99b24d4ba1e8c0a0daa989ccc5d6a2ba5d52
Shoaib.Ghachi <shoaib.ghachi@hotmail.com>
2021-09-24 11:05:05 +0530

    Push New Project

---
+++ UseGstUtilities/UseGstUtilities/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using GstUtilities;
+namespace UseGstUtilities
+{
+    class Program
+    {
+        //  This program demo. how to refer private assembly.
+        static void Main(string[] args)
+        {
+            GST gst = new GST(10000,10);
+            Console.WriteLine($"GST Amount is:{gst.CalculateGstAmt()}");
+            Console.WriteLine($"Total Product Amount is:{gst.CalculateTotalProAmt()}");
+            Console.ReadLine();
+        }
+    }
+}
